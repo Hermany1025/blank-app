589fba2b6bc23ea1dc794dcf9a87d65e92d50886
Hermany1025 <2012huenhe@gmail.com>
2026-04-30 13:32:59 -0700

    b

--- template_24_FINAL.py
+++ template_24_FINAL.py
@@ -20,7 +20,7 @@ with st.form("Form"):
     st.subheader("Current Sentence Need to be Served")
     col1, col2 = st.columns(2)
     with col1:
-        Year = st.number_input("Year", min_value=0, max_value = 1000, step = 0.5, value = 0)
+        Year = st.number_input("Year", min_value=0.0, max_value = 1000.0, step = 0.5, value = 0.0)
     with col2:
         Penalties = st.radio("Penalties", ["Nothing(Cannot be Served)", "Death Penalty", "Death Reprieve","Life Imprisonment"])
     Case = st.text_area("Case Details", key="details")
