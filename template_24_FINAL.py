82b666186552b5d801627b37a0c574079e93e69b
Hermany1025 <2012huenhe@gmail.com>
2026-05-05 13:24:54 -0700

    hou;;huouho;ouh;h;ouououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououououou

--- template_24_FINAL.py
+++ template_24_FINAL.py
@@ -12,36 +12,32 @@ st.divider()
 client = OpenAI(
     api_key = st.secrets["OPENAIKEY"]
 )
-dictionary = {}
 if "Output" not in st.session_state:
     st.session_state.Output = {}
-
-with st.sidebar:
-    st.header("Pages")
-    btn1 = st.button("Add New Defendant", use_container_width=True)
-    btn2 = st.button("Search by Name or ID", use_container_width=True)
-    page = st.selectbox("Select a page", ["Add New Defendant", "Search by Name or ID"])
-if btn1:
-    page = "Add New Defendant"
-    with st.form("Form"):
-        st.title("Welcome to Hermany's AI Judge Template_24_Final")
-        Name = st.text_input("Your Legal Name", key="name")
-        ID = st.text_input("ID")
-        st.subheader("Current Sentence Need to be Served")
-        col1, col2 = st.columns(2)
-        with col1:
-            Penalties = st.radio("Penalties", ["Nothing(Cannot be Served)", "Death Penalty", "Death Reprieve","Life Imprisonment", "Year Imprisonment"])
-        
-        with col2:
-            Year = st.number_input("Year", min_value=0.0, max_value = 10000.0, step = 0.5, value = 0.0)
-        Case = st.text_area("Case Details If searching for a specific case type", key="details")
-        Submit = st.form_submit_button("Submit")    
-    if Submit:
-        if Name == "" or ID == "" or Case == "" or not (Penalties == "Year Imprisonment" and Year > 0.0):
-            st.error("Please fill in all the required fields.")
-        else:
-            st.success("Thanks for submission, Hermany will judge your case as soon as possible. Please wait patiently.")
-            rule_prompt = '''
+if "dictionary" not in st.session_state:
+    st.session_state.dictionary = {}
+if "btn1" not in st.session_state:
+    st.session_state.btn1 = False
+if "btn2" not in st.session_state:
+    st.session_state.btn2 = False
+if "Name" not in st.session_state:
+    st.session_state.Name = ""
+if "ID" not in st.session_state:
+    st.session_state.ID = ""
+if "Penalties" not in st.session_state:
+    st.session_state.Penalties = ""
+if "Year" not in st.session_state:
+    st.session_state.Year = 0.0
+if "Case" not in st.session_state:
+    st.session_state.Case = ""
+if "Submit" not in st.session_state:
+    st.session_state.Submit = False
+if "search_query" not in st.session_state:
+    st.session_state.search_query = ""
+if "response" not in st.session_state:
+    st.session_state.response = ""
+if "rule_prompt" not in st.session_state:
+    st.session_state.rule_prompt = '''
             a. You are fair
             b. You are impartial
             c. Even though you are helping the defendants, you cannot take pity or pleading. Or lean on the defendants to be purposely decreasing the sentence.
@@ -50,7 +46,8 @@ if btn1:
             f. NO PITY
             g. NO PLEADING
             '''
-            Example = '''
+if "Example" not in st.session_state:
+    st.session_state.Example = '''
             {
                 Result:{
                 },
@@ -59,21 +56,71 @@ if btn1:
 
             }
             '''
-            user_prompt = f'''
-            You are a professional AI Judge. You will help those appeal defendants. But please strictly follow the rule, {rule_prompt}. The defendant's name is {Name}, and ID is {ID}. Based on the defendants current sentence {Year} or {Penalties} and {Case}.
-            STRICTLY USE THE FORMAT OF {Example}.
+if "user_prompt" not in st.session_state:
+    st.session_state.user_prompt = f'''
+            You are a professional AI Judge. You will help those appeal defendants. But please strictly follow the rule, {st.session_state.rule_prompt}. The defendant's name is {st.session_state.Name}, and ID is {st.session_state.ID}. Based on the defendants current sentence {st.session_state.Year} or {st.session_state.Penalties} and {st.session_state.Case}.
+            STRICTLY USE THE FORMAT OF {st.session_state.Example}.
             '''
-            response = client.chat.completions.create(
+
+dictionary = {}
+
+
+with st.sidebar:
+    st.header("Pages")
+    st.session_state.btn1 = st.button("Add New Defendant", use_container_width=True)
+    st.session_state.btn2 = st.button("Search by Name or ID", use_container_width=True)
+if st.session_state.btn1:
+    with st.form("Form"):
+        st.title("Welcome to Hermany's AI Judge Template_24_Final")
+        st.session_state.Name = st.text_input("Your Legal Name", key="name")
+        st.session_state.ID = st.text_input("ID")
+        st.subheader("Current Sentence Need to be Served")
+        col1, col2 = st.columns(2)
+        with col1:
+            st.session_state.Penalties = st.radio("Penalties", ["Nothing(Cannot be Served)", "Death Penalty", "Death Reprieve","Life Imprisonment", "Year Imprisonment"])
+        
+        with col2:
+            st.session_state.Year = st.number_input("Year", min_value=0.0, max_value = 10000.0, step = 0.5, value = 0.0)
+        st.session_state.Case = st.text_area("Case Details If searching for a specific case type", key="details")
+        Submit = st.form_submit_button("Submit")    
+    if Submit:
+        if st.session_state.Name == "" or st.session_state.ID == "" or st.session_state.Case == "" or not (st.session_state.Penalties == "Year Imprisonment" and st.session_state.Year > 0.0):
+            st.error("Please fill in all the required fields.")
+        else:
+            st.success("Thanks for submission, Hermany will judge your case as soon as possible. Please wait patiently.")
+            st.session_state.rule_prompt = '''
+            a. You are fair
+            b. You are impartial
+            c. Even though you are helping the defendants, you cannot take pity or pleading. Or lean on the defendants to be purposely decreasing the sentence.
+            d. Be FAIR
+            e. BE IMPARTICAL
+            f. NO PITY
+            g. NO PLEADING
+            '''
+            st.session_state.Example = '''
+            {
+                Result:{
+                },
+                Why do you have the result:{
+                }
+
+            }
+            '''
+            st.session_state.user_prompt = f'''
+            You are a professional AI Judge. You will help those appeal defendants. But please strictly follow the rule, {st.session_state.rule_prompt}. The defendant's name is {st.session_state.Name}, and ID is {st.session_state.ID}. Based on the defendants current sentence {st.session_state.Year} or {st.session_state.Penalties} and {st.session_state.Case}.
+            STRICTLY USE THE FORMAT OF {st.session_state.Example}.
+            '''
+            st.session_state.response = client.chat.completions.create(
                         model="gpt-4o",
                         messages=[
-                            {"role": "user", "content": user_prompt}
+                            {"role": "user", "content": st.session_state.user_prompt}
                         ]
                     )
-            st.write(response.choices[0].message.content)
-            st.write("\n\n",response.choices[0].message.content)
-            st.session_state.Output = json.loads(response.choices[0].message.content)
-            st.session_state.Output[Name] = st.session_state.Output
-            dictionary = st.session_state.Output
+            st.write(st.session_state.response.choices[0].message.content)
+            st.write("\n\n",st.session_state.response.choices[0].message.content)
+            st.session_state.Output = json.loads(st.session_state.response.choices[0].message.content)
+            st.session_state.Output[st.session_state.Name] = st.session_state.Output
+            st.session_state.dictionary = st.session_state.Output
             col1, col2 = st.columns(2)
             with col1:
                 st.subheader("Why do you have the result")
@@ -82,20 +129,19 @@ if btn1:
                 st.subheader("Result")
                 st.write(st.session_state.Output["Result"])
     # st.write(st.session_state.Output)
-if btn2:
-    page = "Search by Name or IDs"
+if st.session_state.btn2:
     if len(st.session_state.Output) == 0:
         st.error("There is no wrongly convicted case in the database, please add new defendants to the database.")
     else:
-        search_query = st.text_input("Search by Name or ID")
-        if search_query:
-            if search_query in st.session_state.Output:
+        st.session_state.search_query = st.text_input("Search by Name or ID")
+        if st.session_state.search_query:
+            if st.session_state.search_query in st.session_state.Output:
                 col1, col2 = st.columns(2)
                 with col1:
                     st.subheader("Why do you have the result")
-                    st.write(st.session_state.Output[search_query]["Why do you have the result"])
+                    st.write(st.session_state.Output[st.session_state.search_query]["Why do you have the result"])
                 with col2:
                     st.subheader("Result")
-                    st.write(st.session_state.Output[search_query]["Result"])
+                    st.write(st.session_state.Output[st.session_state.search_query]["Result"])
             else:
                 st.error("No results found for the given Name or ID.")
