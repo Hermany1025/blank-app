d1598cd3b866fd63c34611afbf9e68e7b0ba7c4c
Hermany1025 <2012huenhe@gmail.com>
2026-05-06 12:27:42 -0700

    Hitler

--- template_24_FINAL.py
+++ template_24_FINAL.py
@@ -84,6 +84,7 @@ if st.session_state.btn1:
         st.session_state.Case = st.text_area("Case Details If searching for a specific case type", key="details")
         Submit = st.form_submit_button("Submit")    
     if Submit:
+        st.write("Hitler is coming, Hermany is judging your case, please wait patiently.")
         if st.session_state.Name == "" or st.session_state.ID == "" or st.session_state.Case == "" or not (st.session_state.Penalties == "Year Imprisonment" and st.session_state.Year > 0.0):
             st.error("Please fill in all the required fields.")
         else:
